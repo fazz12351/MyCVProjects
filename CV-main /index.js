94378c10396c0ced130b0935775f39158d5e6d1b
Mohammed Ahmed <mahme029@gold.ac.uk>
2022-10-10 15:44:16 +0100

    added public folder so static pages can be hosted onto local host

--- CV-main /index.js
+++ CV-main /index.js
@@ -2,6 +2,9 @@ const express=require("express");
 const app=express();
 
 
+app.use(express.static("public"))
+
+
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/myPersonalSite/index.html")
 })
