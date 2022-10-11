9f7ee22d96992f2674695a5998268fc021643e9a
Mohammed Ahmed <mahme029@gold.ac.uk>
2022-10-11 17:01:42 +0100

    indexfile has been hidden

--- CV-main /public/projects/WeatherApp/app.js
+++ CV-main /public/projects/WeatherApp/app.js
@@ -1,25 +1,21 @@
-const express=require("express");
-const bodyParser=require("body-parser");
+const express = require("express");
+const bodyParser=require("body-parser")
 const https = require("https");
-const app=express();
+const app = express();
 
-
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/myPersonalSite/index.html")
-})
-
-app.get("/WeatherAPI",function(req,res){
-    res.sendFile(__dirname+"/public/projects/WeatherApp/index.html")
-    
-    
+    res.sendFile(__dirname+"/index.html")
 })
 
 
-app.post("/WeatherAPI", function (req, res) {
+
+
+app.post("/", function (req, res) {
 
     let city = req.body.City
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=eab3b1e97790f2926281b24b608fd6c7&units=metric";
@@ -46,16 +42,6 @@ app.post("/WeatherAPI", function (req, res) {
 })
 
 
-
-app.get("/projects"+"/WeatherApp",function(req,res){
-   console.log("we are up and running")
-    
-    
+app.listen(3000, function () {
+    console.log("server is running ")
 })
-
-app.listen(3000,function(){
-    console.log("we are running")
-  
-})
-
-
